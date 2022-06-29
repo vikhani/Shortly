2c64892b559e40fead04f8f4796df77a4136c381
vikhani <vikhanieva@gmail.com>
2022-06-29 11:29:12 +0300

    Fix unsupported in Java 11 'toList()' calls

--- src/main/java/com/vkhani/Shortly/Utils/Utils.java
+++ src/main/java/com/vkhani/Shortly/Utils/Utils.java
@@ -4,26 +4,35 @@ import org.apache.commons.validator.routines.UrlValidator;
 
 import java.util.*;
 import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class Utils {
     private Utils() {
     }
 
     public static boolean isUrlValid(String url) {
-        UrlValidator validator = new UrlValidator();
+        UrlValidator validator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES + UrlValidator.ALLOW_LOCAL_URLS);
         return validator.isValid(url);
     }
 
     public static String shortenURL() {
         List<Character> possibleVals = new ArrayList<>();
-        possibleVals.addAll(Arrays.stream(IntStream.rangeClosed(48, 57).toArray()).mapToObj(x -> (char) x).toList());
-        possibleVals.addAll(Arrays.stream(IntStream.rangeClosed(65, 90).toArray()).mapToObj(x -> (char) x).toList());
-        possibleVals.addAll(Arrays.stream(IntStream.rangeClosed(97, 122).toArray()).mapToObj(x -> (char) x).toList());
+        // 0..9
+        possibleVals.addAll(IntStream.rangeClosed(48, 57)
+                .mapToObj(x -> (char) x)
+                .collect(Collectors.toCollection(ArrayList::new)));
+        // A..Z
+        possibleVals.addAll(IntStream.rangeClosed(65, 90)
+                .mapToObj(x -> (char) x)
+                .collect(Collectors.toCollection(ArrayList::new)));
+        // a..z
+        possibleVals.addAll(IntStream.rangeClosed(97, 122)
+                .mapToObj(x -> (char) x)
+                .collect(Collectors.toCollection(ArrayList::new)));
 
         int targetStringLength = 5;
         Random random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
-
         for (int i = 0; i < targetStringLength; i++) {
             char randomLimitedInt = possibleVals.get(random.nextInt(possibleVals.size()));
             buffer.append(randomLimitedInt);
