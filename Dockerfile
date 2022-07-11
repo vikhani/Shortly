d89f651baa049e2c009d48745cf961cc7bc05e7c
vikhani <vikhanieva@gmail.com>
2022-07-11 20:14:09 +0300

    Changed shortening algo to SHA-256 encryption

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:11-jdk
 VOLUME /tmp
 EXPOSE 80
-ADD . .
+COPY . .
 RUN chmod +x gradlew && ./gradlew bootJar
 ENTRYPOINT ["java","-jar","build/libs/Shortly-0.0.1.jar"]
--- src/main/java/com/vkhani/Shortly/Services/UrlPairsService.java
+++ src/main/java/com/vkhani/Shortly/Services/UrlPairsService.java
@@ -2,16 +2,19 @@ package com.vkhani.Shortly.Services;
 
 import com.vkhani.Shortly.Utils.Utils;
 import com.vkhani.Shortly.Models.UrlPair;
-import com.vkhani.Shortly.Repositories.UrlPairsRepository;
 import com.vkhani.Shortly.dtos.UrlPairDto;
-import com.vkhani.Shortly.exceptions.BrokenUrlPairException;
+import com.vkhani.Shortly.Repositories.UrlPairsRepository;
 import com.vkhani.Shortly.exceptions.InvalidURLException;
+import com.vkhani.Shortly.exceptions.BrokenUrlPairException;
+import com.vkhani.Shortly.exceptions.ShorteningUrlException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class UrlPairsService {
@@ -21,20 +24,9 @@ public class UrlPairsService {
     SequenceGeneratorService sequenceGeneratorService;
 
     public UrlPair createUrlPair(String longUrl) {
-        if (!Utils.isUrlValid(longUrl))
-            throw new InvalidURLException("Invalid URL: " + longUrl);
+        if (!Utils.isUrlValid(longUrl)) throw new InvalidURLException("Invalid URL: " + longUrl);
 
-        String shortCode;
-        int maxAttempts = 5;
-        int counter = 0;
-        do {
-            shortCode = Utils.shortenURL();
-            counter++;
-
-            if (counter == maxAttempts && repo.findByShortcutCode(shortCode) != null)
-                throw new BrokenUrlPairException("Unable to generate unique short code");
-
-        } while (repo.findByShortcutCode(shortCode) != null);
+        String shortCode = produceShortCode(longUrl);
 
         UrlPair newPair = new UrlPair(longUrl, shortCode, false);
         newPair.setId(sequenceGeneratorService.generateSequence(UrlPair.SEQUENCE_NAME));
@@ -42,12 +34,12 @@ public class UrlPairsService {
         List<UrlPair> pairs = repo.findAll();
         pairs.add(newPair);
         repo.saveAll(pairs);
+
         return newPair;
     }
 
     public UrlPair createCustomUrlPair(String longUrl, String customShort) {
-        if (!Utils.isUrlValid(longUrl))
-            throw new InvalidURLException("Invalid URL: " + longUrl);
+        if (!Utils.isUrlValid(longUrl)) throw new InvalidURLException("Invalid URL: " + longUrl);
 
         UrlPair newPair = new UrlPair(longUrl, customShort, true);
         newPair.setId(sequenceGeneratorService.generateSequence(UrlPair.SEQUENCE_NAME));
@@ -76,9 +68,30 @@ public class UrlPairsService {
             throw new BrokenUrlPairException("One of the URL pair elements turned out to be empty.");
         }
 
-        final String baseUrl =
-                ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        final String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
 
         return new UrlPairDto(urlPair.getLongURL(), baseUrl + '/' + urlPair.getShortcutCode());
+    }
+
+    private String produceShortCode(String longUrl) {
+        String shortCode;
+        int maxAttempts = 5;
+        int counter = 0;
+
+        do {
+            try {
+                shortCode = Utils.shortenURL(longUrl);
+            } catch (NoSuchAlgorithmException e) {
+                throw new ShorteningUrlException("Unable to produce shortening part for " + longUrl + "url.", e);
+            }
+
+            counter++;
+
+            if (counter == maxAttempts && repo.findByShortcutCode(shortCode) != null)
+                throw new ShorteningUrlException("Unable to generate unique short code");
+
+        } while (repo.findByShortcutCode(shortCode) != null);
+
+        return shortCode;
     }
 }
